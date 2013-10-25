c590cf6e442d68fc04d5075054dc4d10e4d51269
joanpuigsanz <joanpuigsanz@gmail.com>
2013-10-25 20:19:36 +0200

    First commit

---
+++ android/TasksProject/Tasks Example/src/main/java/com/beyondar/android/util/task/example/MainActivity.java
@@ -0,0 +1,15 @@
+package com.beyondar.android.util.task.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
